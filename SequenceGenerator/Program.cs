2ac8c02dcf9a12b0d21a0edfdb2ccd14b0b876a8
Tatsu011 <cmcleod18@hotmail.com>
2020-05-01 11:58:28 -0700

    Initial commit with just the dice.

---
+++ SequenceGenerator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequenceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //a few options for args.
+
+            //-G "Name": generates a new table with the given name. additional parameters- -d num
+
+            //-C "Name": generates a new character with the current table set.
+
+            //-M count: generate a number of characters, each interwoven into other characters. -- will be done later.
+
+            //Lets roll some dice to start.
+            Roll roll = new Roll();
+
+            roll.Die = Dice.d6;
+            Console.WriteLine($"Now rolling {roll}");
+            Console.WriteLine(roll.PerformRoll());
+            Console.WriteLine($"Minimum roll:{roll.GetMinValue()} Maximum roll:{roll.GetMaxValue()} Range: {roll.GetRangeDifference()}");
+
+            roll.Die = Dice.d10;
+            roll.Count = 2;
+            Console.WriteLine($"Now rolling {roll}");
+            Console.WriteLine(roll.PerformRoll());
+            Console.WriteLine($"Minimum roll:{roll.GetMinValue()} Maximum roll:{roll.GetMaxValue()} Range: {roll.GetRangeDifference()}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SequenceGenerator/Table.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequenceGenerator
+{
+    class Table
+    {
+        public Roll dicecheck;
+
+        public Result[] results;
+
+
+
+    }
+
+    public enum Dice
+    {
+        coin = 2, d3,d4,d5,d6,d8 = 8,d10 = 10,d12 = 12,d20 = 20
+    }
+}
