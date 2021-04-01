15ed625a772619237ce9dd794c71b196afe50a27
Cameron McLeod <cmcleod18@hotmail.com>
2021-04-01 12:55:44 -0700

    Cleaned up using statements in all files. Updated libraries (because apparently that hasn't been done in a while)

--- SequenceGenerator/JsonController.cs
+++ SequenceGenerator/JsonController.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.IO;
-using Castle.Components.DictionaryAdapter.Xml;
 
 namespace SequenceGenerator
 {
